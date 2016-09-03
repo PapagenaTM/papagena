f6a116ea1a0f41b7816cbd4bc55c999e40818b64
toni-a <tonia.kaeckmeister@gmx.de>
2016-09-03 20:17:47 +0200

    background color change

---
+++ main.js
@@ -0,0 +1,16 @@
+var background = document.querySelector(".container");
+
+background.addEventListener("click", function() {
+changeBackground();
+});
+
+function changeBackground() {
+var red = getRandomNumber();
+var green = getRandomNumber();
+var blue = getRandomNumber();
+background.style.background = "rgb("+red+","+green+","+blue+")";
+}
+
+function getRandomNumber() {
+return Math.floor(Math.random()*255);
+}
