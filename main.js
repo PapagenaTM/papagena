49e81d1d355662808ec194a895e710038f39e1bc
Malte Modrow <modrow@ubilabs.net>
2018-09-05 17:03:17 +0200

    stuff

--- main.js
+++ main.js
@@ -1,28 +1,33 @@
-var background = document.querySelector(".container");
+var background = document.querySelector('.container');
 
-background.addEventListener("click", function() {
-  changeBackground();
-});
+// background.addEventListener("click", function() {
+//   changeBackground();
+// });
 
 function changeBackground() {
   var red = getRandomNumber();
   var green = getRandomNumber();
   var blue = getRandomNumber();
 
-  background.style.background = "rgb(" + red + "," + green + "," + blue + ")";
+  background.style.background = 'rgb(' + red + ',' + green + ',' + blue + ')';
 }
 
 function getRandomNumber() {
   return Math.floor(Math.random() * 255);
 }
 
-
 if ('serviceWorker' in navigator) {
-  navigator.serviceWorker.register('sw.js').then(function(registration) {
-    // Registration was successful
-    console.log('ServiceWorker registration successful with scope: ', registration.scope);
-  }).catch(function(err) {
-    // registration failed :(
-    console.log('ServiceWorker registration failed: ', err);
-  });
+  navigator.serviceWorker
+    .register('sw.js')
+    .then(function(registration) {
+      // Registration was successful
+      console.log(
+        'ServiceWorker registration successful with scope: ',
+        registration.scope
+      );
+    })
+    .catch(function(err) {
+      // registration failed :(
+      console.log('ServiceWorker registration failed: ', err);
+    });
 }
