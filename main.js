d51a34f927661f03f68fb8c9a7fc65efd2291274
Malte Modrow <modrow@ubilabs.net>
2019-09-05 12:04:27 +0200

    4

--- main.js
+++ main.js
@@ -16,18 +16,18 @@ function getRandomNumber() {
   return Math.floor(Math.random() * 255);
 }
 
-if ('serviceWorker' in navigator) {
-  navigator.serviceWorker
-    .register('sw.js')
-    .then(function(registration) {
-      // Registration was successful
-      console.log(
-        'ServiceWorker registration successful with scope: ',
-        registration.scope
-      );
-    })
-    .catch(function(err) {
-      // registration failed :(
-      console.log('ServiceWorker registration failed: ', err);
-    });
-}
+// if ('serviceWorker' in navigator) {
+//   navigator.serviceWorker
+//     .register('sw.js')
+//     .then(function(registration) {
+//       // Registration was successful
+//       console.log(
+//         'ServiceWorker registration successful with scope: ',
+//         registration.scope
+//       );
+//     })
+//     .catch(function(err) {
+//       // registration failed :(
+//       console.log('ServiceWorker registration failed: ', err);
+//     });
+// }
